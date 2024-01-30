0050d5ca56d93047c9ed4022e4bf63f3b345aca0
Felagin <artyom.tchernyaeff@yandex.ru>
2024-01-30 21:20:01 +0300

    +player

---
+++ backend/app/api2/static/script_player.js
@@ -0,0 +1,113 @@
+const track = document.getElementById("track");
+const thumbnail = document.getElementById("thumbnail");
+const background = document.getElementById("background");
+const trackArtist = document.getElementById("track-artist");
+const trackTitle = document.getElementById("track-title");
+const progressBar = document.getElementById("progressBar");
+const currentTime = document.getElementById("currentTime");
+const durationTime = document.getElementById("durationTime");
+
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let next = document.getElementById("next-track");
+let prev = document.getElementById("prev-track");
+trackIndex = 0;
+// треки+обложки альбомов//
+tracks = [
+  
+];
+thumbnails = [
+  
+];
+trackArtists = ["Harry Styles", "Justin Bieber", "Loud Luxury ft. Brando"];
+trackTitles = ["Watermelon Sugar", "Yummy", "Body"];
+
+let playing = true;
+
+function pausePlay() {
+  if (playing) {
+    play.style.display = "none";
+    pause.style.display = "block";
+
+    thumbnail.style.transform = "scale(1.25)";
+
+    track.play();
+    playing = false;
+  } else {
+    pause.style.display = "none";
+    play.style.display = "block";
+
+    thumbnail.style.transform = "scale(1)";
+
+    track.pause();
+    playing = true;
+  }
+}
+
+play.addEventListener("click", pausePlay);
+pause.addEventListener("click", pausePlay);
+
+track.addEventListener("ended", nextTrack);
+
+function nextTrack() {
+  trackIndex++;
+  if (trackIndex > tracks.length - 1) {
+    trackIndex = 0;
+  }
+
+  track.src = tracks[trackIndex];
+  thumbnail.src = thumbnails[trackIndex];
+  background.src = thumbnails[trackIndex];
+
+  trackArtist.textContent = trackArtists[trackIndex];
+  trackTitle.textContent = trackTitles[trackIndex];
+
+  playing = true;
+  pausePlay();
+}
+
+next.addEventListener("click", nextTrack);
+
+function prevTrack() {
+  trackIndex--;
+  if (trackIndex < 0) {
+    trackIndex = tracks.length - 1;
+  }
+
+  track.src = tracks[trackIndex];
+  thumbnail.src = thumbnails[trackIndex];
+  background.src = thumbnails[trackIndex];
+
+  trackArtist.textContent = trackArtists[trackIndex];
+  trackTitle.textContent = trackTitles[trackIndex];
+
+  playing = true;
+  pausePlay();
+}
+
+prev.addEventListener("click", prevTrack);
+
+function progressValue() {
+  progressBar.max = track.duration;
+  progressBar.value = track.currentTime;
+
+  currentTime.textContent = formatTime(track.currentTime);
+  durationTime.textContent = formatTime(track.duration);
+}
+
+setInterval(progressValue, 500);
+
+function formatTime(sec) {
+  let minutes = Math.floor(sec / 60);
+  let seconds = Math.floor(sec - minutes * 60);
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  return `${minutes}:${seconds}`;
+}
+
+function changeProgressBar() {
+  track.currentTime = progressBar.value;
+}
+
+progressBar.addEventListener("click", changeProgressBar);
