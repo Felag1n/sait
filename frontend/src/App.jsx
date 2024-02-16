c0c9f308043aae48f0a5414decb228bdd6ef26c0
Felagin <artyom.tchernyaeff@yandex.ru>
2024-02-16 17:02:53 +0300

    body start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <Header />
       <body>
-        <h1>Музыка</h1>
+      <h1 style={{ textAlign: "center"}}>Музыка</h1>
       
       <div>
 
@@ -22,6 +22,7 @@ function App() {
         <div className='box'>Lorem.</div>
       </div>
       </body>
+      
     </>
 
   );
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
-
+import Logo from "../Logo/Logo"
+import styles from "../Footer/"
 function Footer(){  
-    return(
-        <footer>
-             
+    return (
+        <footer className={styles.footer}>
+          <div className={styles['footer-container']}>
+            <ul className={styles['footer-links']}>
+              <li><a href="#">Музыка</a></li>
+              <li><a href="#">Чарты</a></li>
+              <li><a href="#">Избранное</a></li>
+            </ul>
+          </div>
         </footer>
-    )
-
-}
+      );
+    };
 export default Footer
