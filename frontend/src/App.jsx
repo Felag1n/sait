b149faa022dcc0734acdfcb454869f08076950a7
Felagin <artyom.tchernyaeff@yandex.ru>
2024-02-18 01:27:27 +0300

    nothing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,24 @@ function App() {
       <Header />
       <body>
       <h1 style={{ textAlign: "center"}}>Музыка</h1>
-      
+      <h2 style={{ textAlign: "center"}}>Обзор</h2>
+      <h3 style={{ textAlign: "center"}}>Интересные теги</h3>
       <div>
 
       </div>
+      <div className='group'>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+        <div className='box'>Lorem.</div>
+      </div>
+      <h4 style={{ textAlign: "center"}}>Последниие релизы</h4>
+      
       <div className='group'>
         <div className='box'>Lorem.</div>
         <div className='box'>Lorem.</div>
