1f915e7977b03d90e3c207586cbc47cdcd8af9fe
Felagin <artyom.tchernyaeff@yandex.ru>
2024-04-06 02:40:23 +0300

    WOW 😎

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,12 @@
+import ServicesTwo from "../components/About/ServicesTwo"
+import ServicesThree from "../components/About/ServicesThree"
 import Services from "../components/About/Services"
 export default function About() {
     return (
         <div>
           <Services/>
+          <ServicesTwo/>
+          <ServicesThree/>
         </div>
     )
 }
